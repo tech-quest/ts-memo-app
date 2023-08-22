4e3ffd0284ec510fcc163d378ba1c7e22ac861c4
kuro-xx <k.kuroda@miwaza.co.jp>
2023-08-22 20:33:09 +0900

    feat: Add simple GET API

---
+++ api/dummy-database/memos.ts
@@ -0,0 +1,23 @@
+export const memosDatabase = [
+  {
+    id: '1',
+    title: 'TypeScriptの学習',
+    content: 'TypeScriptの学習をする\n- 変数\n- 配列\n- 関数',
+    created_at: '2023-08-22 20:05:03',
+    updated_at: '2023-08-22 20:05:03',
+  },
+  {
+    id: '2',
+    title: 'TypeScriptの復習',
+    content: 'TypeScriptの復習をする\n- 変数\n- 配列\n- 関数',
+    created_at: '2023-08-23 20:05:03',
+    updated_at: '2023-08-23 20:05:03',
+  },
+  {
+    id: '3',
+    title: 'フレームワークの学習',
+    content: 'フレームワークの学習をする\n- 変数\n- 配列\n- 関数',
+    created_at: '2023-08-24 20:05:03',
+    updated_at: '2023-08-24 20:05:03',
+  },
+];
--- api/server.ts
+++ api/server.ts
@@ -1,10 +1,47 @@
 import express from 'express';
+import { memosDatabase } from './dummy-database/memos';
 
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// http://localhost:8000/memos
+app.get('/memos', (req, res) => {
+  const memos = memosDatabase.map((memo) => {
+    return {
+      id: memo.id,
+      title: memo.content,
+      createdAt: memo.created_at,
+    };
+  });
+
+  res.json(memos);
+});
+
+// http://localhost:8000/memo/1
+// http://localhost:8000/memo/a -> 404 Not Found
+app.get('/memo/:id', (req, res) => {
+  const id = req.params.id;
+
+  const memo = memosDatabase.find((memo) => {
+    return memo.id === id;
+  });
+
+  if (!memo) {
+    res.status(404).json({ message: 'メモが見つかりませんでした' });
+    return;
+  }
+
+  res.json({
+    id: memo.id,
+    title: memo.title,
+    content: memo.content,
+    createdAt: memo.created_at,
+    updatedAt: memo.updated_at,
+  });
 });
 
 app.listen(port, () => {
