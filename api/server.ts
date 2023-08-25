fd8856dd37eff925eb2b248a1cefd36af4b56fb4
kuro-xx <k.kuroda@miwaza.co.jp>
2023-08-25 10:16:09 +0900

    feat: Add codes that read id memo from static data

--- api/server.ts
+++ api/server.ts
@@ -25,6 +25,36 @@ app.get('/memos', (req, res) => {
   res.json({ data: memos });
 });
 
+// APIのURL http://localhost:8000/memos/1
+// 存在しないIDを指定した場合 http://localhost:8000/memos/a -> 404 Not Found
+// 作成が完了したら http://localhost:3000/detail/1 にアクセスして確認してみましょう！
+app.get('/memos/detail/:id', (req, res) => {
+  const id = Number(req.params.id);
+  if (Number.isNaN(id)) {
+    res.status(404).json({ error: { message: 'ID 形式が不正な形式となっています' } });
+    return;
+  }
+
+  const memo = memosDatabase.find((memo) => {
+    return memo.id === id;
+  });
+
+  if (!memo) {
+    res.status(404).json({ message: 'メモが見つかりませんでした' });
+    return;
+  }
+
+  res.json({
+    data: {
+      id: memo.id,
+      title: memo.title,
+      content: memo.content,
+      createdAt: memo.created_at,
+      updatedAt: memo.updated_at,
+    },
+  });
+});
+
 // ↑↑↑ バックエンド処理を記述して実際に開発してみましょう！！
 
 app.listen(port, () => {
