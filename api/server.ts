27ac1cefba8dbc32ea7b2ddb481696e2365f1e2e
kuro-xx <k.kuroda@miwaza.co.jp>
2023-08-25 09:32:59 +0900

    feat: Add codes that read memos from static data

--- api/server.ts
+++ api/server.ts
@@ -1,6 +1,6 @@
-import { PrismaClient } from '@prisma/client';
 import express from 'express';
 
+import { memosDatabase } from './dummy-database/memos';
 import { applyServerSettings } from './settings';
 
 const app = express();
@@ -10,6 +10,20 @@ const port = 8000;
 applyServerSettings(app);
 
 // ↓↓↓ バックエンド処理を記述して実際に開発してみましょう！！
+
+// APIのURL http://localhost:8000/memos
+// 作成が完了したら http://localhost:3000 にアクセスして確認してみましょう！
+app.get('/memos', (req, res) => {
+  const memos = memosDatabase.map((memo) => {
+    return {
+      id: memo.id,
+      title: memo.title,
+      createdAt: memo.created_at,
+    };
+  });
+
+  res.json(memos);
+});
 
 // ↑↑↑ バックエンド処理を記述して実際に開発してみましょう！！
 
