5c64e413ced06d691363506fdca754c5370ac4c7
kuro-xx <k.kuroda@miwaza.co.jp>
2023-11-06 16:20:05 +0900

    feature: データベースをsqliteに変更

---
+++ prisma/migrations/20231106064605_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "memos" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
