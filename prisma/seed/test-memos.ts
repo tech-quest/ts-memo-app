4d39ec92d0d078af54b1aeb625510033eccb406e
kuro-xx <k.kuroda@miwaza.co.jp>
2023-11-06 16:23:31 +0900

    feature: データベースをsqliteに変更

--- prisma/seed/test-memos.ts
+++ prisma/seed/test-memos.ts
@@ -3,20 +3,22 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const createTestMemos = async () => {
-  await prisma.memo.createMany({
-    data: [
-      {
-        title: 'TypeScriptの学習',
-        content: 'TypeScriptの学習をする\n- 変数\n- 配列\n- 関数\n etc...',
-      },
-      {
-        title: 'TypeScriptの復習',
-        content: 'TypeScriptの復習をする\n- 変数\n- 配列\n- 関数\n etc...',
-      },
-      {
-        title: 'フレームワークの学習',
-        content: 'フレームワークの学習をする\n- 変数\n- 配列\n- 関数\n etc...',
-      },
-    ],
+  await prisma.memo.create({
+    data: {
+      title: 'TypeScriptの学習',
+      content: 'TypeScriptの学習をする\n- 変数\n- 配列\n- 関数\n etc...',
+    },
+  });
+  await prisma.memo.create({
+    data: {
+      title: 'TypeScriptの復習',
+      content: 'TypeScriptの復習をする\n- 変数\n- 配列\n- 関数\n etc...',
+    },
+  });
+  await prisma.memo.create({
+    data: {
+      title: 'フレームワークの学習',
+      content: 'フレームワークの学習をする\n- 変数\n- 配列\n- 関数\n etc...',
+    },
   });
 };
